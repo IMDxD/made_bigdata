34e5f9cd65b0be14ce7659a6945b3727bca0d5e4
IMDxD <nestor969@mail.ru>
2021-03-14 19:24:31 +0300

    hw2 fixes

--- hw2/reducer.py
+++ hw2/reducer.py
@@ -4,14 +4,14 @@ import sys
 import random
 
 
-r = random.randint(1, 6)
+r = random.randint(1, 5)
 tmp = []
 
 for line in sys.stdin:
     tmp.append(line.rstrip().split("_")[1])
     if len(tmp) == r:
         print(",".join(tmp))
-        r = random.randint(1, 6)
+        r = random.randint(1, 5)
         tmp = []
 
 if len(tmp) != 0:
--- hw2/run.sh
+++ hw2/run.sh
@@ -4,11 +4,9 @@ set -x
 input_ids_hdfs_path=/data/ids
 output_hdfs_path=hw2_mr_data_ids
 job_name=random
-output_file=hw2_mr_data_ids.out
 HADOOP_STREAMING_JAR=/usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming.jar
 
 hdfs dfs -rm -r $output_hdfs_path
-rm -rf $output_file
 
 yarn jar $HADOOP_STREAMING_JAR \
     -files mapper.py,reducer.py \
@@ -16,4 +14,7 @@ yarn jar $HADOOP_STREAMING_JAR \
     -output $output_hdfs_path \
     -mapper "mapper.py" \
     -numReduceTasks 2 \
-    -reducer "reducer.py" > $output_file
+    -reducer "reducer.py"
+
+hdfs dfs -cat $output_hdfs_path/part-00000 | head -50
+
